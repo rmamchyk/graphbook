7b03826df78a9a93865629c113584ced61926d9a
rmamchyk <roman.mamchyk@gmail.com>
2019-11-02 15:25:12 +0200

    express server setup which exposes graphql endpoint

---
+++ src/server/services/graphql/schema.js
@@ -0,0 +1,37 @@
+
+const typeDefinitions = `
+  type Post {
+    id: Int
+    text: String
+    user: User
+  }
+
+  type User {
+    avatar: String
+    username: String
+  }
+
+  input PostInput {
+    text: String!
+  }
+
+  input UserInput {
+    username: String!
+    avatar: String!
+  }
+
+  type RootQuery {
+    posts: [Post]
+  }
+
+  type RootMutation {
+    addPost(post: PostInput!, user: UserInput!): Post
+  }
+  
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+`;
+
+export default [typeDefinitions];
