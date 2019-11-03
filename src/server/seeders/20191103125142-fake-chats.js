a4ed8218c4542c960afdc7c46e4a67e4dd0068f2
rmamchyk <roman.mamchyk@gmail.com>
2019-11-03 15:26:20 +0200

    connecting to mysql database using sequelize

---
+++ src/server/seeders/20191103125142-fake-chats.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Chats', [
+      {
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Chats', null, {});
+  }
+};
--- src/server/services/graphql/schema.js
+++ src/server/services/graphql/schema.js
@@ -1,14 +1,28 @@
 
 const typeDefinitions = `
+  type User {
+    id: Int
+    avatar: String
+    username: String
+  }
+
   type Post {
     id: Int
     text: String
     user: User
   }
 
-  type User {
-    avatar: String
-    username: String
+  type Message {
+    id: Int
+    text: String
+    chat: Chat
+    user: User
+  }
+
+  type Chat {
+    id: Int
+    messages: [Message]
+    users: [User]
   }
 
   input PostInput {
@@ -20,12 +34,25 @@ const typeDefinitions = `
     avatar: String!
   }
 
+  input ChatInput {
+    users: [Int]
+  }
+
+  input MessageInput {
+    text: String!
+    chatId: Int!
+  }
+
   type RootQuery {
     posts: [Post]
+    chats: [Chat]
+    chat(chatId: Int): Chat
   }
 
   type RootMutation {
-    addPost(post: PostInput!, user: UserInput!): Post
+    addPost(post: PostInput!): Post
+    addChat (chat: ChatInput!): Chat
+    addMessage (message: MessageInput!): Message
   }
   
   schema {
